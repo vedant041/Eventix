38bb43f9640861ff4644dd074af3f0bef79c19e4
vedant041 <121607731+vedant041@users.noreply.github.com>
2025-01-27 14:59:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import dotenv from "dotenv";
+import messageRouter from "./router/messageRouter.js";
+import cors from "cors";
+
+const app = express();
+
+dotenv.config({ path: "./config/.env" });
+
+app.use(
+  cors({
+    origin:  'http://localhost:5173',
+    methods: ["POST"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/message", messageRouter);
+
+dbConnection();
+
+export default app;
